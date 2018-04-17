2887004a9cb69eb0a86aef45219aedc41e769a98
Admin <Admin@192.168.0.6>
2018-04-18 00:35:18 +0530

    xz

--- src/prg/ajay/myWorld/AjaysWorld.java
+++ src/prg/ajay/myWorld/AjaysWorld.java
@@ -3,7 +3,7 @@ package prg.ajay.myWorld;
 public class AjaysWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Ajay World--removed Ajay updated");
+		System.out.println("Ajay World--Ajay updated");
 
 	}
 
