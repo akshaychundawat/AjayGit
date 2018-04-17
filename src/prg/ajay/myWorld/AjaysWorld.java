b08660b865f86051d21b36c8a0f5e18881946c37
Admin <Admin@192.168.0.6>
2018-04-17 22:20:03 +0530

    First Commit

---
+++ src/prg/ajay/myWorld/AjaysWorld.java
@@ -0,0 +1,10 @@
+package prg.ajay.myWorld;
+
+public class AjaysWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Ajay World");
+
+	}
+
+}
---
+++ src/prg/ajay/myWorld/HelloWorld.java
@@ -0,0 +1,10 @@
+package prg.ajay.myWorld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Testing Git World");
+
+	}
+
+}
